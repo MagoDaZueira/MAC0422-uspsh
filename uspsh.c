f389260e2252c152e782353a1e9b4023324440c5
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2025-03-12 22:10:10 -0300

    uspsh finalizado

--- uspsh.c
+++ uspsh.c
@@ -1,3 +1,5 @@
+/******************************************************************
+***************************** INCLUDES ***************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,7 +12,11 @@
 #include <sys/wait.h>
 
 
+/******************************************************************
+********************** GERENCIAMENTO DE INPUT ********************/
+
 // Cria um prompt seguindo a formatação desejada ("[name:dir]$ ")
+// Retorna uma string contendo esse prompt
 char *create_prompt(char *hostname, size_t hsize, char *directory, size_t dsize) {
     // Atribui valores aos buffers
     gethostname(hostname, hsize);
@@ -20,6 +26,7 @@ char *create_prompt(char *hostname, size_t hsize, char *directory, size_t dsize)
     size_t size = strlen(hostname) + strlen(directory) + strlen("[]:$ ") + 1;
     char *prompt = malloc(size);
 
+    // Formatação correta
     strcpy(prompt, "[");
     strcat(prompt, hostname);
     strcat(prompt, ":");
@@ -29,11 +36,14 @@ char *create_prompt(char *hostname, size_t hsize, char *directory, size_t dsize)
     return prompt;
 }
 
+// Conta quantos partes separadas por espaço (" ") o input tem.
+// Retorna essa quantia.
 int count_args(char *str) {
     char copy[strlen(str) + 1];
     char *token;
     int count = 0;
 
+    // Itera sobre uma cópia do input, contando
     strcpy(copy, str);
     token = strtok(copy, " ");
     while (token != NULL) {
@@ -44,15 +54,18 @@ int count_args(char *str) {
     return count;
 }
 
-
+// Separa as partes do input, delimitadas por espaço (" ").
+// Retorna um vetor com as partes em questão.
 char **separate_args(char *input) {
     char **args;
     char *token;
     int count;
     
+    // Reserva memória de acordo com a quantia de argumentos
     count = count_args(input);
     args = malloc((count + 1) * sizeof(char *));
     
+    // Preenche o vetor args com as partes separadas por espaço do input
     int i = 0;
     token = strtok(input, " ");
     while (token != NULL) {
@@ -64,38 +77,48 @@ char **separate_args(char *input) {
     return args;
 }
 
+/******************************************************************
+************************ COMANDOS INTERNOS ***********************/
+
+// Muda o diretório atual
 void cd(char **args) {
-    if (args[1] != NULL)
-        chdir(args[1]);
-    else
+    if (args[1] == NULL) {
         fprintf(stderr, "Uso correto: cd <diretorio>\n");
+        return;
+    }
+    // Syscall
+    chdir(args[1]);
 }
 
+// Mostra o user id efetivo
 void whoami() {
+    // Syscalls para obter o nome do usuário
     uid_t id = geteuid();
     struct passwd *pw = getpwuid(id);
 
     printf("%s\n", pw->pw_name);
 }
 
+// Altera as permissões de um dado arquivo ou diretório
 void ch_mod(char **args) {
     if (args[1] == NULL || args[2] == NULL) {
         fprintf(stderr, "Uso correto: chmod <permissao> <arquivo|diretorio>\n");
         return;
     }
 
+    // Converte string para octal
     char *endptr;
     mode_t permission = strtol(args[1], &endptr, 8);
     char *path = args[2];
 
+    // Syscall
     chmod(path, permission);
 }
 
-int is_programa_bin(char *command) {
-    return !strncmp(command, "/bin", 4) ||
-           !strncmp(command, "/usr", 4);
-}
+/******************************************************************
+************************ PROGRAMAS EXTERNOS **********************/
 
+// Executa programas externos (fork() + execve())
 void do_execve(char **args) {
     pid_t pid = fork(); // Novo processo
 
@@ -110,26 +133,19 @@ void do_execve(char **args) {
     }
 }
 
+// A partir dos argumentos do input, executa o que foi solicitado
 void execute_args(char **args) {
     char *command = args[0];
 
-    if (!strcmp(command, "cd")) {
-        cd(args);
-    }
-    else if (!strcmp(command, "whoami")) {
-        whoami();
-    }
-    else if (!strcmp(command, "chmod")) {
-        ch_mod(args);
-    }
-    else if (is_programa_bin(command)) {
-        do_execve(args);
-    }
-    else if (!strcmp(command, "bye")) {
-        exit(0);
-    }
+    if      (!strcmp(command, "cd"))     cd(args);
+    else if (!strcmp(command, "whoami")) whoami();
+    else if (!strcmp(command, "chmod"))  ch_mod(args);
+    else if (!strcmp(command, "bye"))    exit(0);
+    else                                 do_execve(args);
 }
 
+/******************************************************************
+*************************** FUNÇÃO MAIN **************************/
 int main() {
     // Cria buffers para o prompt e para o input
     char hostname[256];
